8d08df623368b72a0fb33c7ca5374a0d937311df
owennguyen3162 <nguyentuan3162@gmail.com>
2024-05-05 21:39:53 +0700

    Initial Commit

---
+++ Movie-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie-App
+//
+//  Created by Nguyen Anh Tuan on 5/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
